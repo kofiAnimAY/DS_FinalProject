60bffcc3eb743cfe6667c11bdd467e9d48b9bb99
INdaka <in2223@nyu.edu>
2026-04-15 19:21:55 +0200

    Fix: remove ydata-profiling incompatible with Python 3.14

--- ml_project.py
+++ ml_project.py
@@ -16,13 +16,7 @@ import altair as alt
 from htbuilder import HtmlElement, div, hr, a, p, img, styles
 from htbuilder.units import percent, px
 
-# Replace these:
-# import pandas_profiling
-# from streamlit_pandas_profiling import st_profile_report
 
-# With these:
-from ydata_profiling import ProfileReport
-from streamlit_ydata_profiling import st_profile_report
 
 
 
@@ -199,14 +193,12 @@ if app_mode == 'Introduction':
 
     st.markdown("### 04 - Complete Report")
     if st.button("Generate Report"):
-
-        # Replace this:
-        # pr = df.profile_report()
-        
-        # With this:
-        pr = ProfileReport(df, explorative=True)
-        
-        st_profile_report(pr)
+        st.subheader("Dataset Overview")
+        st.write(df.describe(include='all'))
+        st.subheader("Missing Values")
+        st.write(df.isnull().sum())
+        st.subheader("Data Types")
+        st.write(df.dtypes)
 
 
 if app_mode == 'Visualization':
